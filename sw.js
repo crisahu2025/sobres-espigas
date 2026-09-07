4a0ec3d756caa8a57196ec76519ded411f9e1416
Code Ahumada <cris.ahu777@gmail.com>
2026-09-07 16:11:56 -0300

    chore: conectar nueva URL segura de Apps Script y actualizar cache v1.0.7

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker para Sobres Gran Rey PWA
-const CACHE_NAME = 'granrey-sobres-v1.0.6';
+const CACHE_NAME = 'granrey-sobres-v1.0.7';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html?v=1.0.6',
+  './index.html?v=1.0.7',
   './escudo.png',
   './favicon.png',
   './manifest.json'
