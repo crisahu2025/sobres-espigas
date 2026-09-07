0d2976fef0d9124464fb7958e2b303c490a2a52b
Code Ahumada <cris.ahu777@gmail.com>
2026-09-07 14:48:33 -0300

    style: eliminar bloque de titulo redundante, agregar user-select none y actualizar cache v1.0.3

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker para Sobres Gran Rey PWA
-const CACHE_NAME = 'granrey-sobres-v1.0.2';
+const CACHE_NAME = 'granrey-sobres-v1.0.3';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html?v=1.0.2',
+  './index.html?v=1.0.3',
   './escudo.png',
   './favicon.png',
   './manifest.json'
